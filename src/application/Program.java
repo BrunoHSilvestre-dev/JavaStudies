45da106c0caf292708dc5dc35b1904583899c57e
BrunoHSilvestre-dev <brunosilvestre201010@gmail.com>
2024-08-08 18:20:03 -0300

    Interface exercise base resolution

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,15 +3,7 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-//		FilesSource.execute();
-		
-//		FilesSource.fileReader();
-		
-//		FilesSource.fileWriter();
-		
-//		FilesSource.manipulatingFolders();
-		
-		FilesSource.exercise();
+		InterfacesSource.exercise();
 	}
 
 }
