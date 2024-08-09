d0ce09b848815c5158b1abeb5221b76ba729a021
BrunoHSilvestre-dev <brunosilvestre201010@gmail.com>
2024-08-08 21:43:58 -0300

    Example of an implementation of the 'comparable' interface

--- src/application/InterfacesSource.java
+++ src/application/InterfacesSource.java
@@ -1,8 +1,15 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
+import entities.Employee;
 import model.entities.CarRental;
 import model.entities.Vehicle;
 import model.services.BrazilTaxService;
@@ -27,5 +34,24 @@ public class InterfacesSource {
 		System.out.println(carRental.getInvoice());
 		
 		sc.close();
+	}
+	
+	public static void intefaceComparable() {
+		List<Employee> list = new ArrayList<>();
+		String path = "D:\\ProgramFiles\\Java\\EclipseWorkspace\\Files\\names.txt";
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employeeCsv = br.readLine();
+			}
+			Collections.sort(list);
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", " + emp.getSalary());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 }
