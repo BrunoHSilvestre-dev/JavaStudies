e4bf32baa6d625650a8e14e1a891bf1151cc0fe9
BrunoHSilvestre-dev <brunosilvestre201010@gmail.com>
2024-08-12 18:55:17 -0300

    HashCode and Equals

---
+++ src/application/HashCodeEqualsSource.java
@@ -0,0 +1,21 @@
+package application;
+
+import entities.Client;
+
+public class HashCodeEqualsSource {
+
+	public static void execute() {
+		String a = "Maria";
+		String b = "Alex";
+		System.out.println(a.equals(b));
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+		
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Alex", "alex@gmail.com");
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+	}
+}
