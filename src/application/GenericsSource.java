11e84d7a54a699493530454f2776183bb6c922f0
BrunoHSilvestre-dev <brunosilvestre201010@gmail.com>
2024-08-12 18:36:23 -0300

    Bounded wildcards

--- src/application/GenericsSource.java
+++ src/application/GenericsSource.java
@@ -1,9 +1,13 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import entities.shapes.Circle;
+import entities.shapes.Rectangle;
+import entities.shapes.Shape;
 import model.services.PrintService;
 
 public class GenericsSource {
@@ -36,9 +40,75 @@ public class GenericsSource {
 		Integer x = 10;
 		obs = x;
 	
-		List<?> myObjs = new ArrayList<Object>();
+		List<?> myObjs = new ArrayList<Object>(); // List<?> is the supertype of any List
 		List<Integer> myNumbers = new ArrayList<Integer>();
 		myObjs = myNumbers; 
 	}
 
+	public void printList(List<?> list) {
+	    //using a wildcard type to print any type of list
+		for (Object elem : list) {
+	        System.out.println(elem);
+	    }
+	}
+	
+	public static void shapeBoundedWildCard(String[] args) {
+		
+		List<Shape> myShapes = new ArrayList<>();
+		myShapes.add(new Rectangle(3.0, 2.0));
+		myShapes.add(new Circle(2.0));
+		
+		System.out.println("Total area: " + totalArea(myShapes));
+		
+		List<Circle> myCircles = new ArrayList<>();
+		myCircles.add(new Circle(2.0));
+		myCircles.add(new Circle(3.0));
+		
+		System.out.println("Total area: " + totalArea(myCircles));
+	}
+	
+	public static double totalArea(List<? extends Shape> list) {
+		//the wildcard with extends Shape allows to enter only shape extended classes as a parameter
+		
+		double sum = 0.0;
+		for (Shape s : list) {
+			sum += s.area();
+		}
+		return sum;
+	}
+	
+	public static void getPutPrinciple() {
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(10);
+		intList.add(5);
+		
+		List<? extends Number> list = intList;
+		
+		Number x = list.get(0);
+		
+//		list.add(20); compilation error
+		
+		List<Object> myObjs = new ArrayList<Object>();
+		myObjs.add("Maria");
+		myObjs.add("Alex");
+		List<? super Number> myNums = myObjs;
+		myNums.add(10);
+		myNums.add(3.14);
+//		Number y = myNums.get(0); compilation error
+	}
+
+	public static void copyList() {
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
+		
+		copy(myInts, myObjs);
+		copy(myDoubles, myObjs);
+	}
+	
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for(Number number : source) {
+			destiny.add(number);
+		}
+	}
 }
---
+++ src/entities/shapes/Shape.java
@@ -0,0 +1,5 @@
+package entities.shapes;
+
+public interface Shape {
+	double area();
+}
