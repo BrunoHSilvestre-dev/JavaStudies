dc6496c629b5f7e2a749b1667954dba87951ba9b
BrunoHSilvestre-dev <brunosilvestre201010@gmail.com>
2024-08-15 17:55:06 -0300

    Comparator, lambda expression and 'arrow functions'

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,13 +3,7 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-//		SetSource.execute();
-		
-//		SetSource.treeSet();
-		
-//		SetSource.setExercise();
-		
-		SetSource.map();
+		ComparatorSource.execute();
 	}
 
 }
